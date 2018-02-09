30f05948098ff59d497aa7c733b948eaaafa7e56
JIALEI GAO <35751071+gao540166030@users.noreply.github.com>
2018-02-09 23:43:11 +0800

    Add files via upload

--- Code/main.py
+++ Code/main.py
@@ -1,28 +1,9 @@
 import sys
 import time  
 import Adafruit_DHT
-import RPi.GPIO as GPIO 
-
-# Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
-sensor = sensor_args['11']
-DHT11pin = 4
-
-
-#Temperature  Humidity
-def GetTH():
-    time.sleep(1)
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11pin)
-    if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    else:
-        print('Failed to get reading. Try again!')
-        sys.exit(1)
-    time.sleep(1)
+import RPi.GPIO as GPIO     
     
-    
+DataForQT = []
 def init():  
     GPIO.setwarnings(False)  
     GPIO.setmode(GPIO.BOARD) 
@@ -41,6 +22,8 @@ def init():
     GPIO.setup(31,GPIO.OUT)
     GPIO.setup(32, GPIO.IN)
     
+    
+    
     GPIO.output(33, GPIO.LOW)
     GPIO.output(35, GPIO.LOW) 
     GPIO.output(37, GPIO.LOW)
@@ -48,11 +31,43 @@ def init():
     GPIO.output(38, GPIO.LOW) 
     GPIO.output(40, GPIO.LOW)
     
-    GPIO.output(31, GPIO.LOW)
+    GPIO.output(31, GPIO.HIGH)
     
     pass  
   
+  
+# Parse command line parameters.
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+sensor = sensor_args['11']
+DHT11pin = 4
 
+
+#Temperature  Humidity
+def GetTH():
+    time.sleep(1)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11pin)
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        DataForQT.append(temperature)  
+        DataForQT.append(humidity)
+    else:
+        print('Failed to get reading. Try again!')
+        sys.exit(1)
+    
+    if temperature <= 25:
+        LED(1)
+        GPIO.output(31, GPIO.HIGH)
+    elif temperature <= 30:
+        LED(2)
+        GPIO.output(31, GPIO.LOW)
+    elif temperature > 30:
+        LED(3)
+        GPIO.output(31, GPIO.LOW)
+    time.sleep(1)
+    
+    
 #LED      
 def G1_LED():
     GPIO.output(33, GPIO.HIGH)
@@ -84,6 +99,11 @@ def R2_LED():
     GPIO.output(36, GPIO.LOW)
     GPIO.output(38, GPIO.LOW) 
     GPIO.output(40, GPIO.HIGH)
+    
+def Y2R2_LED():
+    GPIO.output(36, GPIO.LOW)
+    GPIO.output(38, GPIO.HIGH) 
+    GPIO.output(40, GPIO.HIGH)
 
 def LED(argument):
     switcher = {
@@ -94,6 +114,7 @@ def LED(argument):
         4: G2_LED,
         5: Y2_LED,
         6: R2_LED,
+        7: Y2R2_LED,
     }
     func = switcher.get(argument)
     return func()
@@ -107,27 +128,45 @@ def beep():
             time.sleep(0.5)  
             print "the Buzzer will make sound"  
 
-#HC-SR501            
-def HC_detct():  
+#HC-SR501 + Slant           
+def detct():  
     #for i in range(1, 31):  
     if GPIO.input(12) == True:  
         print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Someone is closing!"  
-        beep()  
+        LED(5)
+        DataForQT.append(1)
+        if GPIO.input(32) == False:
+            print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Something is slanting!"
+            LED(7)
+            DataForQT.append(1)
+            beep()  
+        else:
+            DataForQT.append(0)
+            
     else:  
         GPIO.output(11, GPIO.HIGH)  
         print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Noanybody!"  
-    time.sleep(4) 
-
-#Slant   
-def SL_detct():
-    if GPIO.input(32) == True:
-        LED(6)
+        DataForQT.append(0)
+        if GPIO.input(32) == False:
+            print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Something is slanting!"
+            LED(6)
+            DataForQT.append(1)
+            beep()  
+        else:
+            LED(4)
+            DataForQT.append(0)
+    time.sleep(2) 
         
 init()  
 LED(1)
 LED(4)
              
 while True:
+    
+    DataForQT = []
+    
     GetTH()
-#    time.sleep(2)
-    HC_detct()
+    detct()
+    
+    print DataForQT
+    
